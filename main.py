d5bc6c75e876cb7f63f76e46447c893702948793
Cuantocos <santiagoortiz0609@hotmail.com>
2022-12-11 01:41:20 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import operaciones as o
+
+resSuma = o.suma(10,110)
+resResta = o.resta(5,3)
+resMultiplicar = o.multiplicar(5,5)
+resDividir = o.dividir(25, 5)
+
+print(resSuma, resResta, resMultiplicar, resDividir)
